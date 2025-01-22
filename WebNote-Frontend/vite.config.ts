efa1001349492ebf8d9889930135b167eb95f4c6
SihakVidya <samsihakvityea@gmail.com>
2025-01-23 01:21:23 +0700

    tst

--- WebNote-Frontend/vite.config.ts
+++ WebNote-Frontend/vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
   base: "/WebNoteFull/",
   build: {
-    outDir: "docs",
+    outDir: "../docs", // Build to docs in root folder
   },
 });
