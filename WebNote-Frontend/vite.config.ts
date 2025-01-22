911ae32e28797cbba2381d2e369734594c866f82
SihakVidya <samsihakvityea@gmail.com>
2025-01-23 01:15:23 +0700

    t

--- WebNote-Frontend/vite.config.ts
+++ WebNote-Frontend/vite.config.ts
@@ -4,5 +4,8 @@ import vue from "@vitejs/plugin-vue";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "/WebNote-Frontend/",
+  base: "/WebNoteFull/",
+  build: {
+    outDir: "docs",
+  },
 });
